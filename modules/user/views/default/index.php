6edae4f7b73b10bc8a1f72658ed85ccb0a7cb954
Yaroslav Skrypnyk <yar.skrypnyk@gmail.com>
2020-01-17 20:14:16 +0200

    liqpay is working

--- modules/user/views/default/index.php
+++ modules/user/views/default/index.php
@@ -4,4 +4,22 @@
     <p>Id покупки: <?= $item->id ?></p>
     <p>Цена: <?= $item->price ?></p>
     <p>----------</p>
+
+    <?php
+    $liqpay = new LiqPay('sandbox_i62814999345', 'sandbox_1jLzUiOmPrM60AiZF0lWO5XuDYW5oNkQnkqf3gWo');
+    $html = $liqpay->cnb_form(array(
+        'action'         => 'pay',
+        'amount'         => $item->price,
+        'currency'       => 'UAH',
+        'description'    => 'Test payment',
+        'order_id'       => $item->id,
+        'version'        => '3'
+    ));
+
+    ?>
+        <?php
+        echo $html;
+        ?>
+
+
 </div>
