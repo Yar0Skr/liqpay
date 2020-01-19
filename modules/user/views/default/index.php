42c1dba94274f20e4a149a6b2a7fea391d20a028
Yaroslav <51512763+Yar0Skr@users.noreply.github.com>
2020-01-19 11:33:27 +0200

    Update index.php

--- modules/user/views/default/index.php
+++ modules/user/views/default/index.php
@@ -6,7 +6,7 @@
     <p>----------</p>
 
     <?php
-    $liqpay = new LiqPay('sandbox_i62814999345', 'sandbox_1jLzUiOmPrM60AiZF0lWO5XuDYW5oNkQnkqf3gWo');
+    $liqpay = new LiqPay('publick_key', 'private_key');
     $html = $liqpay->cnb_form(array(
         'action'         => 'pay',
         'amount'         => $item->price,
